f4e51c46545035db223c872ad4f80b37cb3b2df0
Donald mxolisi Mohlala <donald.mxolisi@proton.me>
2024-06-12 18:37:21 +0200

    new

--- src/components/Navbar/Join.jsx
+++ src/components/Navbar/Join.jsx
@@ -1,9 +1,12 @@
 import React, { useState , useEffect} from 'react';
+import axios from 'axios';
 import './Join.scss';
 
 const Modal = ({ isOpen, onClose }) => {
   const [gameId, setGameId] = useState('');
   const [isButtonDisabled, setIsButtonDisabled] = useState(true);
+  const[Loading , setIsLoading]=useState(false);
+  const token = localStorage.getItem("token");
 
   useEffect(() => {
     setIsButtonDisabled(
@@ -14,11 +17,30 @@ const Modal = ({ isOpen, onClose }) => {
     setGameId(e.target.value);
   };
 
-  const handleJoinClick = () => {
-    console.log(`Joining game with ID: ${gameId}`);
-    
-    onClose(); 
+  const handleJoinClick = async () => {
+    setIsLoading(true);
+  
+    try {
+      const response = await axios.post( 
+        "https://play929-1e88617fc658.herokuapp.com/games/JoinGame",
+        {
+          gameId: gameId 
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`
+          }
+        }
+      );
+      window.location.href=response.data.Link;
+  
+    } catch (err) {
+      
+    }
+  
+   setIsLoading(false);
   };
+  
 
   if (!isOpen) {
     return null;
@@ -34,7 +56,7 @@ const Modal = ({ isOpen, onClose }) => {
           onChange={handleInputChange}
           placeholder="Enter Game ID"
         />
-        <button onClick={handleJoinClick} className='form_btn' disabled={isButtonDisabled}>Join</button>
+        <button onClick={handleJoinClick} className='form_btn' disabled={Loading ||isButtonDisabled}>{Loading ? 'Joining...' : 'Join'}</button>
         <button className="close-button" onClick={onClose}>Close</button>
       </div>
     </div>
